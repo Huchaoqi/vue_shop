6b47e6e1ebd0a4a843a1c05a1817658c961901da
超奇 <252261109@qq.com>
2019-12-19 20:39:12 +0800

    登录功能跳转完整以及退出登录

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -63,7 +63,8 @@ export default {
         const { data: res } = await this.$http.post('login', this.loginForm)
         if (res.meta.status !== 200) return this.$message.error('登录失败!')
         this.$message.success('登录成功')
-        window.sessionStorage.setItem('token', res.datatoken)
+        // console.log(res)
+        window.sessionStorage.setItem('token', res.data.token)
         this.$router.push('/home')
       })
     }
