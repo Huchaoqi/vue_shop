4224a06a6858037a14bbd317e4c7ab59229d3494
超奇 <252261109@qq.com>
2019-12-20 14:43:01 +0800

    侧边栏折叠功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,41 +3,39 @@
     <!-- 页面头部 -->
     <el-header>
       <div>
-        <img src="../assets/heima.png" alt="">
+        <img src="../assets/heima.png" alt="" />
         <span>电商后台管理系统</span>
       </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
     <el-container>
-       <!-- 侧边栏内容 -->
-       <el-aside width="200px">
-         <!-- 侧边栏菜单部分 -->
-    <el-menu
-      background-color="#333744"
-      text-color="#fff"
-      active-text-color="#ffd04b">
-      <!-- 一级菜单 -->
-      <el-submenu index="1">
-        <!-- 一级菜单模板 -->
-        <template slot="title">
-          <!-- 图标 -->
-          <i class="el-icon-location"></i>
-          <!-- 文本 -->
-          <span>导航一</span>
-        </template>
-        <!-- 二级菜单 -->
-        <el-menu-item index="1-4-1">
-          <template slot="title">
-          <!-- 图标 -->
-          <i class="el-icon-location"></i>
-          <!-- 文本 -->
-          <span>导航一</span>
-        </template>
-        </el-menu-item>
-      </el-submenu>
-    </el-menu>
-    </el-aside>
+      <!-- 侧边栏内容 -->
+      <el-aside :width="isCollapse ?'64px':'200px'">
+        <div class="toggle_button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单部分 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" :unique-opened="true" :collapse="isCollapse" :collapse-transition="false">
+          <!-- 一级菜单 -->
+          <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+            <!-- 一级菜单模板 -->
+            <template slot="title">
+              <!-- 图标 -->
+              <i :class="iconsObj[item.id]"></i>
+              <!-- 文本 -->
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1" v-for="subItem in item.children" :key="subItem.id">
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-menu"></i>
+                <!-- 文本 -->
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <!-- 右侧内容主体 -->
       <el-main>Main</el-main>
     </el-container>
@@ -46,14 +44,38 @@
 
 <script>
 export default {
-  data() {
-    return {}
+  created() {
+    this.getMemuList()
   },
-  created() {},
+  data() {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      iconsObj: {
+        '125': 'iconfont icon-duanxiu',
+        '103': 'iconfont icon-zhibei',
+        '101': 'iconfont icon-zuanshi',
+        '102': 'iconfont icon-bi',
+        '145': 'iconfont icon-xin'
+      },
+      isCollapse: false
+    }
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    // 获取所有菜单
+    async getMemuList() {
+      const { data: res } = await this.$http.get('menus')
+      // console.log(res)
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(res)
+    },
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
@@ -67,21 +89,37 @@ export default {
   background-color: #333744;
   display: flex;
   justify-content: space-between;
-  padding-left:0;
+  padding-left: 0;
   align-items: center;
   color: #fff;
   font-size: 20px;
-  >div {
+  > div {
     display: flex;
     align-items: center;
-
   }
-
+  span {
+    margin-left: 20px;
+  }
 }
 .el-aside {
   background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
 }
 .el-main {
-  background-color: #EAEDF1;
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle_button {
+  background-color: #4a5064;
+  font-style: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
 }
 </style>
