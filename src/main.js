86124f77b2a6799d2295f666a252833c9b34e2a0
超奇 <252261109@qq.com>
2019-12-20 11:59:42 +0800

    主页菜单布局

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './assets/css/global.css'
 // 导入axios请求
 import axios from 'axios'
 // 配置请求根路径
-axios.defaults.baseURL = `http://127.0.0.1:8888/api/private/v1/`
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 // 挂载到Vue原型上
 Vue.prototype.$http = axios
 
